15b8b50bcfd17702b726035648b3d55dfc502911
techlife hacking <techlife.hacking@gmail.com>
2020-12-04 00:59:03 +0900

    modify

--- mainRaspberryPi.py
+++ mainRaspberryPi.py
@@ -14,9 +14,11 @@ import requests
 import blynklib
 
 BLYNK_AUTH = 'BW-OY7yiT4hVfh2BzXR_x3xy23j7Nkyr'
+RaspberryPiIPAdress = 'YourRaspberryPiIPAdress'
 
 # initialize Blynk
 blynk = blynklib.Blynk(BLYNK_AUTH)
+
 
 class Threadblynk(threading.Thread):
     def __init__(self):
@@ -27,6 +29,8 @@ class Threadblynk(threading.Thread):
             blynk.run()
 
 # register handler for virtual pin V0 write event
+
+
 @blynk.handle_event('write V0')
 def write_virtual_pin_handler(pin, value):
     if value == ['1']:
@@ -34,25 +38,32 @@ def write_virtual_pin_handler(pin, value):
         tvstate.changingtimer()
 
 # register handler for virtual pin V2 write event
+
+
 @blynk.handle_event('write V2')
 def write_virtual_pin_handler(pin, value):
     if value == ['1']:
-        requests.post('http://192.168.100.134:8080/api/module/youtube/youtubeload',
-                    headers=headers, data=dataPlayload0)
+        requests.post('http://'+RaspberryPiIPAdress+':8080/api/module/youtube/youtubeload',
+                      headers=headers, data=dataPlayload0)
 
 # register handler for virtual pin V3 write event
+
+
 @blynk.handle_event('write V3')
 def write_virtual_pin_handler(pin, value):
     if value == ['1']:
-        requests.post('http://192.168.100.134:8080/api/module/youtube/youtubeload',
-                    headers=headers, data=dataPlayload1)
+        requests.post('http://'+RaspberryPiIPAdress+':8080/api/module/youtube/youtubeload',
+                      headers=headers, data=dataPlayload1)
 
 # register handler for virtual pin V4 write event
+
+
 @blynk.handle_event('write V4')
 def write_virtual_pin_handler(pin, value):
     if value == ['1']:
-        requests.post('http://192.168.100.134:8080/api/module/youtube/youtubecontrol',
-                    headers=headers, data=dataPause)
+        requests.post('http://'+RaspberryPiIPAdress+':8080/api/module/youtube/youtubecontrol',
+                      headers=headers, data=dataPause)
+
 
 class TVState():
     def turnTV(self):
@@ -60,13 +71,14 @@ class TVState():
 
     def changingtimer(self):
         raise NotImplementedError("changingtimer is abstractmethod")
-    
+
     def turnYouTube(self):
         raise NotImplementedError("turnYouTube is abstractmethod")
 
+
 class TVON(TVState):
     def turnTV(self):
-        requests.post('http://192.168.100.134:8080/api/module/youtube/youtubecontrol',
+        requests.post('http://'+RaspberryPiIPAdress+':8080/api/module/youtube/youtubecontrol',
                       headers=headers, data=dataPause)
         os.system('echo "standby 0" | cec-client -s')
         tvstate.change_state("tvon2off")
@@ -77,6 +89,7 @@ class TVON(TVState):
     def turnYouTube(self):
         youtubestate.turnYouTube()
         youtubestate.changingtimer()
+
 
 class TVON2OFF(TVState):
     def turnTV(self):
@@ -89,9 +102,10 @@ class TVON2OFF(TVState):
     def turnYouTube(self):
         pass
 
+
 class TVOFF(TVState):
     def turnTV(self):
-        requests.post('http://192.168.100.134:8080/api/module/youtube/youtubecontrol',
+        requests.post('http://'+RaspberryPiIPAdress+':8080/api/module/youtube/youtubecontrol',
                       headers=headers, data=dataPlay)
         os.system('echo "on 0" | cec-client -s')
         tvstate.change_state("tvoff2on")
@@ -101,6 +115,7 @@ class TVOFF(TVState):
 
     def turnYouTube(self):
         pass
+
 
 class TVOFF2ON(TVState):
     def turnTV(self):
@@ -112,6 +127,7 @@ class TVOFF2ON(TVState):
 
     def turnYouTube(self):
         pass
+
 
 class TVContext:
     def __init__(self):
@@ -131,7 +147,8 @@ class TVContext:
         elif switchsignal == "tvoff2on":
             self.state = self.tvoff2on
         else:
-            raise ValueError("change_state method must be in {}".format(["tvon", "tvon2off", "tvoff", "tvoff2on"]))
+            raise ValueError("change_state method must be in {}".format(
+                ["tvon", "tvon2off", "tvoff", "tvoff2on"]))
 
     def turnTV(self):
         self.state.turnTV()
@@ -142,6 +159,7 @@ class TVContext:
     def turnYouTube(self):
         self.state.turnYouTube()
 
+
 class YouTubeState():
     def turnYouTube(self):
         raise NotImplementedError("turnYouTube is abstractmethod")
@@ -149,14 +167,16 @@ class YouTubeState():
     def changingtimer(self):
         raise NotImplementedError("changingtimer is abstractmethod")
 
+
 class YouTubeON(YouTubeState):
     def turnYouTube(self):
-        requests.post('http://192.168.100.134:8080/api/module/youtube/youtubecontrol',
+        requests.post('http://'+RaspberryPiIPAdress+':8080/api/module/youtube/youtubecontrol',
                       headers=headers, data=dataPause)
         youtubestate.change_state("youtubeon2off")
 
     def changingtimer(self):
         pass
+
 
 class YouTubeON2OFF(YouTubeState):
     def turnYouTube(self):
@@ -166,14 +186,16 @@ class YouTubeON2OFF(YouTubeState):
         time.sleep(3)
         youtubestate.change_state("youtubeoff")
 
+
 class YouTubeOFF(YouTubeState):
     def turnYouTube(self):
-        requests.post('http://192.168.100.134:8080/api/module/youtube/youtubecontrol',
+        requests.post('http://'+RaspberryPiIPAdress+':8080/api/module/youtube/youtubecontrol',
                       headers=headers, data=dataPlay)
         youtubestate.change_state("youtubeoff2on")
 
     def changingtimer(self):
         pass
+
 
 class YouTubeOFF2ON(YouTubeState):
     def turnYouTube(self):
@@ -182,6 +204,7 @@ class YouTubeOFF2ON(YouTubeState):
     def changingtimer(self):
         time.sleep(3)
         youtubestate.change_state("youtubeon")
+
 
 class YouTubeContext:
     def __init__(self):
@@ -201,13 +224,15 @@ class YouTubeContext:
         elif switchsignal == "youtubeoff2on":
             self.state = self.youtubeoff2on
         else:
-            raise ValueError("change_state method must be in {}".format(["youtubeon", "youtubeon2off", "youtubeoff", "youtubeoff2on"]))
+            raise ValueError("change_state method must be in {}".format(
+                ["youtubeon", "youtubeon2off", "youtubeoff", "youtubeoff2on"]))
 
     def turnYouTube(self):
         self.state.turnYouTube()
 
     def changingtimer(self):
         self.state.changingtimer()
+
 
 if __name__ == "__main__":
 
@@ -226,11 +251,10 @@ if __name__ == "__main__":
     tvstate = TVContext()
     youtubestate = YouTubeContext()
 
-
     # t = threading.Timer(300.0, tvstate.turnTV())
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('192.168.100.134', 50007))
+        s.bind((''+RaspberryPiIPAdress+'', 50007))
         s.listen(1)
         while True:
             conn, addr = s.accept()
@@ -245,6 +269,3 @@ if __name__ == "__main__":
                         tvstate.changingtimer()
                     else:
                         pass
-
-
-
